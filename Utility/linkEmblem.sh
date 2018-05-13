8a12b00f0fb7f46ddc95bce19b92b4e3bec52c8e
Nattapong Pullkhow <ixenatt@icloud.com>
2018-05-13 10:18:13 +0700

    Add utility

---
+++ Utility/linkEmblem.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-default.svg emblem-default.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-downloads.svg emblem-downloads.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-favorite.svg emblem-favorite.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-generic.svg emblem-generic.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-important.svg emblem-important.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-marketing.svg emblem-marketing.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-money.svg emblem-money.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-new.svg emblem-new.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-people.svg emblem-people.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-personal.svg emblem-personal.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-presentation.svg emblem-presentation.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-readonly.svg emblem-readonly.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-resent.svg emblem-resent.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-shared.svg emblem-shared.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-sound.svg emblem-sound.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-symbolic-link.svg emblem-symbolic-link.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-system.svg emblem-system.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-unreadable.svg emblem-unreadable.svg
+ln -sf ../../Xenlism-Wildfire/Emblems-Day/emblem-urgent.svg emblem-urgent.svg
---
+++ Utility/linkPlaces.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#link places
+ln -sf ../../Xenlism-Wildfire/Places/cs-desktop.svg cs-desktop.svg
+ln -sf ../../Xenlism-Wildfire/Places/document-open-recent-symbolic.svg document-open-recent-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/emptytrash emptytrash
+ln -sf ../../Xenlism-Wildfire/Places/emptytrash.svg emptytrash.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-documents-symbolic.svg folder-documents-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-download-symbolic.svg folder-download-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-music-symbolic2.svg folder-music-symbolic2.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-music-symbolic.svg folder-music-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-pictures-symbolic.svg folder-pictures-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-publicshare-symbolic.svg folder-publicshare-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-recent-symbolic.svg folder-recent-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-remote-symbolic.svg folder-remote-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-symbolic.svg folder-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/folder-videos-symbolic.svg folder-videos-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/gnome-fs-trash.svg gnome-fs-trash.svg
+ln -sf ../../Xenlism-Wildfire/Places/gnome-stock-trash.svg gnome-stock-trash.svg
+ln -sf ../../Xenlism-Wildfire/Places/network-server-symbolic.svg network-server-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/network-workgroup.svg network-workgroup.svg
+ln -sf ../../Xenlism-Wildfire/Places/network-workgroup-symbolic.svg network-workgroup-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/trashcan_empty.svg trashcan_empty.svg
+ln -sf ../../Xenlism-Wildfire/Places/user-desktop.svg user-desktop.svg
+ln -sf ../../Xenlism-Wildfire/Places/user-desktop-symbolic.svg user-desktop-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/user-home-symbolic.svg user-home-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/user-trash-full-symbolic.svg user-trash-full-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/user-trash.svg user-trash.svg
+ln -sf ../../Xenlism-Wildfire/Places/user-trash-symbolic.svg user-trash-symbolic.svg
+ln -sf ../../Xenlism-Wildfire/Places/xfce-trash_empty.svg xfce-trash_empty.svg
+
